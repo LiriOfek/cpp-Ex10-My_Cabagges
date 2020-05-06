8ae84ca578216dc50afcd85f11d9350ff4328b89
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-06 21:18:15 +0300

    add main file with documentation

---
+++ Ex10-My_Cabagges/main.cpp
@@ -0,0 +1,7 @@
+/********************************************************\
+File Name: main.cpp
+Author: Liri (4/4/2020)
+Purpose: This main file contain the main function that run the class that
+			wrap fopen and test it functionality
+\********************************************************/
+
