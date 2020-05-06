6399d848ea23ac5f525b8e34747cc60e651bd00e
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-06 18:17:35 +0300

    add cpp and header file of wrap fopen with documentation

---
+++ Ex10-My_Cabagges/wrap_fopen.cpp
@@ -0,0 +1,8 @@
+/********************************************************\
+File Name: wrap_fopen.cpp
+Author: Liri (4/4/2020)
+Purpose: This file contain class of WrapFopen, with the functions that 
+			run fopen with the given parameters, if the opening is fails 
+			than throw exception, and if the progrem end than close the
+			file automatically
+\********************************************************/
---
+++ Ex10-My_Cabagges/wrap_fopen.h
@@ -0,0 +1,8 @@
+/********************************************************\
+File Name: wrap_fopen.h
+Author: Liri (4/4/2020)
+Purpose: This header file contain class of WrapFopen, with the declaration 
+			of functions that run fopen with the given parameters, if the 
+			opening fails than throw exception, and if the progrem end 
+			than close the file automatically
+\********************************************************/
