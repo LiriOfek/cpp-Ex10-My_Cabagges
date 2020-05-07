a5354d1cf9c9b408a51ae4ca69dfb1259e2755f5
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-07 08:22:31 +0300

    add constructor of WrapFwrite

--- Ex10-My_Cabagges/wrap_fwirte.h
+++ Ex10-My_Cabagges/wrap_fwirte.h
@@ -6,3 +6,29 @@ Purpose: This header file contain class of WrapFwrite, that inherits
 			fwrite with the given parameters, if the writing fails 
 			than throw exception
 \********************************************************/
+
+#pragma once
+
+#include "wrap_fopen.h"
+#include <cstdio>
+
+/*class that wrap fwrite, inherits from WrapFopen,
+	throw exception if writing fail*/
+class WrapFwrite : public WrapFopen {
+public:
+	WrapFwrite(const string& new_file_name, 
+			   const char* new_opening_mode) :
+		WrapFopen(new_file_name, new_opening_mode) {
+	}
+	/**
+	* @brief  create WrapFwriten and set the name of the file and the
+	*			opening mode, by the constructor in WrapFopen
+	* @param  IN const string& new_file_name - the new file name
+	*		  IN const char* new_opening_mode - the opening mode
+	* @return this function has no return value
+	* @note   create WrapFwritten and set the name of the file and the
+	*			opening mode
+	* @author  Liri
+	*/
+
+};
