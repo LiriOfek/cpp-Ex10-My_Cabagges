2ead161839442a04be82ca7ebb2f18e4bd49817c
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-07 08:18:14 +0300

    add cpp and header files of class that wrap fwrite with documentation

---
+++ Ex10-My_Cabagges/wrap_fwirte.h
@@ -0,0 +1,8 @@
+/********************************************************\
+File Name: wrap_fwirte.h
+Author: Liri (4/4/2020)
+Purpose: This header file contain class of WrapFwrite, that inherits 
+			from WrapFopen with the declaration of functions that run 
+			fwrite with the given parameters, if the writing fails 
+			than throw exception
+\********************************************************/
---
+++ Ex10-My_Cabagges/wrap_fwrite.cpp
@@ -0,0 +1,8 @@
+/********************************************************\
+File Name: wrap_fwirte.cpp
+Author: Liri (4/4/2020)
+Purpose: This file contain class of WrapFwrite, that inherits
+			from WrapFopen with the functions that run
+			fwrite with the given parameters, if the writing 
+			fails than throw exception
+\********************************************************/
