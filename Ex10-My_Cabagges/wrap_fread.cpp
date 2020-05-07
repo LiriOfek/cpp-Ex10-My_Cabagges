f4a11719ea13fa60eb61e820e33cfed6a446ecef
LiriOfek <46228594+LiriOfek@users.noreply.github.com>
2020-05-07 09:01:56 +0300

    add cpp and header file of class WrapFread with documentation

---
+++ Ex10-My_Cabagges/wrap_fread.cpp
@@ -0,0 +1,8 @@
+/********************************************************\
+File Name: wrap_fread.cpp
+Author: Liri (4/4/2020)
+Purpose: This file contain class of WrapFread, that inherits
+			from WrapFopen with the functions that run
+			fread with the given parameters, if the reading fails
+			than throw exception
+\********************************************************/
---
+++ Ex10-My_Cabagges/wrap_fread.h
@@ -0,0 +1,8 @@
+/********************************************************\
+File Name: wrap_fread.h
+Author: Liri (4/4/2020)
+Purpose: This header file contain class of WrapFread, that inherits
+			from WrapFopen with the declaration of functions that run
+			fread with the given parameters, if the reading fails
+			than throw exception
+\********************************************************/
